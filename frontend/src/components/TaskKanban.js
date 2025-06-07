f61b2a04462c20421dcffec175791bebcb900be5
Lingam Vamshi Krishna Reddy <125525582+lingamvamshikrishnareddy@users.noreply.github.com>
2025-06-07 22:32:28 +0530

    Update TaskKanban.js

--- frontend/src/components/TaskKanban.js
+++ frontend/src/components/TaskKanban.js
@@ -197,7 +197,8 @@ const TaskKanban = ({
       month: 'short',
       day: 'numeric',
       hour: '2-digit',
-      minute: '2-digit'
+      minute: '2-digit',
+      hour12: true, // Use AM/PM
     });
   };
 
